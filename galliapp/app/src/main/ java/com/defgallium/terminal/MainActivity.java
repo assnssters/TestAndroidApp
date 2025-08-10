a3aedaf00c72b932c104ddd34a774f9ea0a9ed4a
assnssters <dinhductri18@gmail.com>
2025-08-10 12:43:54 +0700

    Update MainActivity.java

--- galliapp/app/src/main/ java/com/defgallium/terminal/MainActivity.java
+++ galliapp/app/src/main/ java/com/defgallium/terminal/MainActivity.java
@@ -1,1 +1,40 @@
+package com.defgallium.terminal;
 
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    private GLSurfaceView glSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        glSurfaceView = new GLSurfaceView(this);
+
+        // Khai báo sử dụng OpenGL ES 2.0
+        glSurfaceView.setEGLContextClientVersion(2);
+
+        // Gắn renderer tùy chỉnh
+        glSurfaceView.setRenderer(new MyRenderer());
+
+        // Render liên tục
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+}
